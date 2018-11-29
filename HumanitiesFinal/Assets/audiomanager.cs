a42ced7f3f24b98b1e1afa123b73e94f0758f460
ncunnin4 <44483651+ncunnin4@users.noreply.github.com>
2018-11-28 22:15:58 -0700

    I tried a thing!

---
+++ HumanitiesFinal/Assets/audiomanager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine.Audio;
+using System;
+using UnityEngine;
+
+public class audiomanager : MonoBehaviour
+{
+
+    public Sound[] sounds;
+
+    // Use this for initialization
+    void Awake()
+    {
+        foreach (Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+
+        }
+    }
+
+    private void Start()
+    {
+        Play("Theme");
+    }
+    public void Play(string name)
+    {
+        Array.Find(sounds, sound => sound.name == name);
+       
+    }
+}
