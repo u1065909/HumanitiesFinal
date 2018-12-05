964974c1b4479496a76fddd5ca5ed1fbaccc911c
joshcheung21 <joshss212312@gmail.com>
2018-12-04 18:51:45 -0700

    More level fixes

--- HumanitiesFinal/Assets/Scripts/Kid.cs
+++ HumanitiesFinal/Assets/Scripts/Kid.cs
@@ -4,30 +4,18 @@ using UnityEngine;
 
 public class Kid : MonoBehaviour {
 
-    public PlayerController playerController;
+    public Transform player;
+    public float timeToWaitThenRun;
     bool calledRight;
 	// Use this for initialization
 	void Start ()
     {
-        playerController = GetComponentInParent<PlayerController>();
-        print(transform.position.x);
-        print(Mathf.Abs(transform.position.x)*-1);
+
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-        if (playerController.facingRight && !calledRight)
-        {
-            transform.localPosition = new Vector3(transform.localPosition.x*-1, transform.localPosition.y);
-            calledRight = true;
-            print("called");
-        }
-        else if(!playerController.facingRight && calledRight)
-        {
-            transform.localPosition = new Vector3(transform.localPosition.x*-1, transform.localPosition.y);
-            calledRight = false;
-            print("called left");
-        }
+        
 	}
 }
